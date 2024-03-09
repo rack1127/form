dc302dde150965d6a96ac625311de3978282e800
rack1127 <rack1127>
2024-03-09 15:52:50 +0900

    音が出るようにした

--- app/app.py
+++ app/app.py
@@ -1,20 +1,30 @@
+import pygame
 import csv
 import json
 import cv2
 from pyzbar.pyzbar import decode
 
+def play_mp3(filename):
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+
 def to_csv(json_dict): 
     json_dict= json.loads(json_dict)   
-    with open('sensor_output.csv', 'a', newline='') as f:
+    with open('sensor_output.csv', 'a+',newline='') as f:
         writer = csv.DictWriter(f, fieldnames=json_dict.keys(), 
                                 doublequote=True, 
                                 quoting=csv.QUOTE_ALL)
         print(writer)
-        writer.writeheader()
+        f.seek(0)
+        if f.read() == "":    
+            writer.writeheader()
         writer.writerow(json_dict)
 
 def capture():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) # カメラ画像の横幅を1280に設定
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) 
     # frame = cv2.imread('qrcode.png', cv2.IMREAD_UNCHANGED)
     tmp = ""
 
@@ -31,12 +41,15 @@ def capture():
 
                 # 値が空でなく、かつ、ひとつ前のデータ同じではないとき
                 if value != "" and value != tmp:
+                    play_mp3('test.mp3')
                     # データを一時保管
                     tmp = value
                     to_csv(value)
 
             # ウィンドウ表示
-            cv2.imshow('frame', frame)
+            cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
+            cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+            cv2.imshow('window', frame)
 
         # Qキー押すと終了
         if cv2.waitKey(1) & 0xFF == ord('q'):
